9546fdf99235ccbdc1fdcd0d63b6671c4c3f485b
kravz2702-byte <kravz2702@gmail.com>
2023-10-12 23:35:01 +0300

    start auth

---
+++ src/api/user/auth/auth.helper.ts
@@ -0,0 +1,55 @@
+import {
+  Injectable,
+  HttpException,
+  HttpStatus,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '@/api/user/user.entity';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthHelper {
+  @InjectRepository(User)
+  private readonly repository: Repository<User>;
+
+  private readonly jwt: JwtService;
+
+  constructor(jwt: JwtService) {
+    this.jwt = jwt;
+  }
+
+  //Decode the JWT token
+  public async decode(token: string): Promise<unknown> {
+    return this.jwt.decode(token, null);
+  }
+
+  //Get user by user id we get from decode()
+  public async validateUser(decoded: any): Promise<User> {
+    return this.repository.findOne(decoded.id);
+  }
+  //Encode user's password
+  public encodePassword(password: string): string {
+    const salt: string = bcrypt.genSaltSync(10);
+
+    return bcrypt.hashSync(password, salt);
+  }
+
+  //Validate JWT token, throw frobidden error if JWT token is invalid
+  private async validate(token: string): Promise<boolean | never> {
+    const decoded: unknown = this.jwt.verify(token);
+
+    if (!decoded) {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
+
+    const user: User = await this.validateUser(decoded);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
